6e653bde696522e2395e44233da2864e981fd98e
VANSHIKA <vanshikaavailable@gmail.com>
2025-05-17 23:22:42 +0530

    Add test API route

--- server.js
+++ server.js
@@ -63,3 +63,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+app.get("/api", (req, res) => {
+  res.send("API is working!");
+});
